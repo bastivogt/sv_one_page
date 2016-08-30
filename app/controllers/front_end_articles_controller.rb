5d060f712db7f2e9ed0fb9408fe2cb2506783243
Sebastian Vogt <s.vogt@cam-store.eu>
2016-08-30 16:10:56 +0200

    Create a navbar menu with bootsstrap-sass

--- app/controllers/front_end_articles_controller.rb
+++ app/controllers/front_end_articles_controller.rb
@@ -1,5 +1,7 @@
 class FrontEndArticlesController < ApplicationController
   def index
     @articles = Article.order(:sorting_index);
+    @nav_items_left = Article.where(menu_position: "left").order(:sorting_index)
+    @nav_items_right = Article.where(menu_position: "right").order(:sorting_index)
   end
 end

d09c1dbd5f3db16a51b1dace300c8a497b069edc
Sebastian Vogt <s.vogt@cam-store.eu>
2016-08-31 18:20:30 +0200

    Added helper create_accordion_panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,30 @@ def create_panel(title, &block)
 end
 
 
-def create_accordion_panel(title: "Title", id: "accordion", &block)
-  s = "<h3>#{title}</h3>"
+def create_accordion_panel(title: "Title", id: "accordion", hl_tag: "h4", open: false, &block)
+  collapsed = ""
+  if open
+    collapsed = " in"
+  end
+  s = <<-eos
+  <div class="panel-group" id="#{id}" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-primary">
+    <div class="panel-heading" role="tab">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="##{id}" href="#collapseEins" aria-expanded="true" aria-controls"collapseEins">
+          #{title}
+        </a>
+      </h4>
+    </div>
+    <div id="collapseEins" class="panel-collapse collapse#{collapsed}" role="tabpanel" aria-labelledby="überschriftEins">
+      <div class="panel-body">
+  eos
   b = capture(&block)
-  e = ""
+  e = <<-eos
+      </div>
+    </div>
+  </div>
+  eos
   result = s + b + e
   result.html_safe
 end

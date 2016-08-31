181164e620f45771f1f5f5fc52e9fc45d30134ac
Sebastian Vogt <s.vogt@cam-store.eu>
2016-08-31 17:37:51 +0200

    Added create_panel helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+
+def create_panel(title, &block)
+  s = "<div class='panel'><h3>#{title}</h3>"
+  b = capture(&block)
+  e = "</div>"
+  r = s + b + e
+  r.html_safe
+end
+
+
+
 end
